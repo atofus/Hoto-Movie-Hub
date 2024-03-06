91eb1c84d1ddd7d3eeff5f36ebbf03f28e77a205
Alan To <alantofus@gmail.com>
2024-03-06 02:48:10 -0800

    Added branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,14 +8,27 @@ import { Route } from 'react-router-dom';
 import Register from './Pages/Register';
 import { Routes } from 'react-router-dom';
 
+//import Navbar from "./Components/navbar"
+import { Home } from "./Pages/Home";
+// import Contact from "./Pages/Contact";
+// import About from "./Pages/About";
+// import WatchList from "./Pages/Watchlist";
+
 
 function App() {
+
+
   return (
     <>
+
       <Router>
         <Routes>
           <Route path="/Login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
+          <Route path="/Home" element= {<Home />} />
+          {/* <Route path="/Contact" element= {<Contact />} />
+          <Route path="/About" element= {<About />} />
+          <Route path="/WatchList" element= {<WatchList />} /> */}
 
         </Routes>
 
